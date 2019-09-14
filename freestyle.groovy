0b07af30fff8711a74bc3574a74de99004da2b91
sindhu-2k19 <vsindhu.2k18@gmail.com>
2019-09-14 02:41:56 -0400

    "task3"

--- freestyle.groovy
+++ freestyle.groovy
@@ -1,4 +1,4 @@
-freeStyleJob('Task2') {
+freeStyleJob('Task3') {
     logRotator(-1, 10)
     jdk('Java 8')
     scm {
@@ -7,6 +7,8 @@ freeStyleJob('Task2') {
                 name('task1')
                 url('git@github.com:vsindhu-2k19/myautomationjenkins.git')
             }
-       }
+       dsl {
+              external('task1.groovy') 
+           }
    }
 }
